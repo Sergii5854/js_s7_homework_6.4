65e4d3a3d11e2a0306ed8bdd01606809e0b6c8e9
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-23 22:46:50 +0200

    little story about life of the developer

--- src/assets/component/gif.jsx
+++ src/assets/component/gif.jsx
@@ -1,94 +1,85 @@
-import React, {Component } from 'react';
-import gif1 from './../gif/feed.gif';
-import gif2 from './../gif/funk.gif';
-import gif3 from './../gif/internet.gif';
-import gif4 from './../gif/sleeping.gif';
-import gif5 from './../gif/study.gif';
-import gif6 from './../gif/watchingTV.gif';
+import React, {Component } from 'react'
+import gif1 from './../gif/sleeping.gif'
+import gif2 from './../gif/funk.gif'
+import gif3 from './../gif/feed.gif'
+import gif4 from './../gif/internet.gif'
+import gif5 from './../gif/watchingTV.gif'
+import gif6 from './../gif/study.gif'
 
 export default class GifActions extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
+      labelSleep: true,
+      colorSleep: false,
+      gifSleep: true,
 
-      labelA: true,
-      colorA: false,
-      actionA: true,
+      labelEat: true,
+      colorEat: false,
+      gifEat: true,
 
-      labelB: true,
-      colorB: false,
-      actionB: true,
-
-      labelC: true,
-      colorC: false,
-      actionC: true,
-    };
-
+      labelWatch: true,
+      colorWatch: false,
+      gifWatch: true,
+    }
 
     this.sleep = this.sleep.bind(this)
     this.eat = this.eat.bind(this)
-    this.wathTV = this.watchTV.bind(this)
+    this.wath = this.watch.bind(this)
   }
-
 
   sleep() {
     this.setState({
-      colorA: !this.state.colorA,
-      labelA: !this.state.labelA,
-      actionA: !this.state.actionA
+      labelSleep: !this.state.labelSleep,
+      colorSleep: !this.state.colorSleep,
+      gifSleep: !this.state.gifSleep
     })
-    let action = this.state.actionA ? gif1 : gif1;
-    document.getElementById("gif").src = action;
+    let actionBtnSleep = this.state.gifSleep ? gif1 : gif2
+    document.getElementById('gif').src = actionBtnSleep
   }
 
   eat() {
     this.setState({
-      colorB: !this.state.colorB,
-      labelB: !this.state.labelB,
-      actionB: !this.state.actionB
+      labelEat: !this.state.labelEat,
+      colorEat: !this.state.colorEat,
+      gifEat: !this.state.gifEat
     })
-    let action2 = this.state.actionB ? gif4 : gif3;
-    document.getElementById("gif").src = action2;
+    let actionBtnEat = this.state.gifEat ? gif3 : gif4
+    document.getElementById('gif').src = actionBtnEat
   }
 
-  watchTV() {
+  watch() {
     this.setState({
-      colorC: !this.state.colorC,
-      labelC: !this.state.labelC,
-      actionC: !this.state.actionC
+      labelWatch: !this.state.labelWatch,
+      colorWatch: !this.state.colorWatch,
+      gifWatch: !this.state.gifWatch
     })
-    let action3 = this.state.actionC ? gif5 : gif6;
-    document.getElementById("gif").src = action3;
+    let actionBtnWatch = this.state.gifWatch ? gif5 : gif6
+    document.getElementById('gif').src = actionBtnWatch
   }
 
   render() {
-    let bgColor = this.state.colorA ? "red" : "green";
-    let click =  this.state.labelA ? 'Sleep' : 'Awake';
-     let action = this.state.actionA ? gif1 : gif2;
+    let clickBtnSleep =  this.state.labelSleep ? 'Sleep' : 'Run'
+    let bgColorBtnSleep = this.state.colorSleep ? 'red' : 'green'
 
-    let bgColor2 = this.state.colorB ? "red" : "green";
-    let click2 =  this.state.labelB? 'Eat' : 'Play';
-    let action2 = this.state.actionB ? gif3 : gif4;
+    let clickBtnEat =  this.state.labelEat? 'Eat' : 'Play'
+    let bgColorBtnEat = this.state.colorEat ? 'grey' : 'saddlebrown'
 
-    let bgColor3 = this.state.colorC ? "red" : "green";
-    let click3 =  this.state.labelC ? 'Watch' : 'Go gym';
-    let action3 = this.state.actionC ? gif5 : gif6;
+    let clickBtnWatch =  this.state.labelWatch ? 'Watch' : 'Study'
+    let bgColorBtnWatch = this.state.colorWatch ? 'blue' : 'purple'
+
     return (
-      <div>
+        <div>
+          <div className='gif-container'>
+            <img id='gif' className='active' alt=''/>
+          </div>
 
-        <div className="gif-container">
-          <img id="gif" className="active" src={action} />
-
+          <div className='controls'>
+            <button id='a' className='btn' style={{backgroundColor: bgColorBtnSleep}} onClick={this.sleep} >{clickBtnSleep}</button>
+            <button id='b' className='btn' style={{backgroundColor: bgColorBtnEat}} onClick={this.eat} > {clickBtnEat} </button>
+            <button id='c' className='btn' style={{backgroundColor: bgColorBtnWatch}} onClick={this.wath} > {clickBtnWatch} </button>
+          </div>
         </div>
-
-        <div className="controls">
-          <button id="a" className="btn" style={{backgroundColor: bgColor}} onClick={this.sleep} >{click}</button>
-          <button id="b" className="btn" style={{backgroundColor: bgColor2}} onClick={this.eat} > {click2} </button>
-          <button id="c" className="btn" style={{backgroundColor: bgColor3}} onClick={this.wathTV} > {click3} </button>
-        </div>
-
-      </div>
-    );
+    )
   }
-};
+}
